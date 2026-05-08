ce05035e77d6be97f29c2e15d1e15e114493ced8
Youn Mélois <youn@melois.dev>
2026-05-08 08:35:03 +0200

    feat: initial commit
    Signed-off-by: Youn Mélois <youn@melois.dev>

---
+++ README.md
@@ -0,0 +1,7 @@
+# minecraft-server-manager
+
+Straightforward tool to manage your minecraft servers.
+
+## License
+
+This project is licensed under either of [`Apache License 2.0`](LICENSE-APACHE) or [`MIT License`](LICENSE-MIT) at your option.
---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "minecraft-server-manager development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      fenix,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+
+      supportedSystems = [
+        "x86_64-linux"
+      ];
+
+      forAllSystems =
+        systems: f:
+        lib.genAttrs systems (
+          system:
+          f (
+            import nixpkgs {
+              inherit system;
+              overlays = [
+                fenix.overlays.default
+              ];
+            }
+          )
+        );
+    in
+    {
+      devShells = forAllSystems supportedSystems (
+        pkgs:
+        let
+          toolchainFile = pkgs.fenix.fromToolchainFile {
+            file = ./rust-toolchain.toml;
+            sha256 = "sha256-gh/xTkxKHL4eiRXzWv8KP7vfjSk61Iq48x47BEDFgfk=";
+          };
+
+          rustToolchain = pkgs.fenix.combine [
+            pkgs.rust-analyzer
+            pkgs.fenix.latest.rustfmt
+            toolchainFile
+          ];
+        in
+        {
+          default =
+            with pkgs;
+            mkShell {
+              nativeBuildInputs = [
+                rustToolchain
+              ];
+            };
+        }
+      );
+    };
+}
